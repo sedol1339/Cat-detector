447e050480b21004f5d5bc99c97de524b912d33a
Oleg Sedukhin <sedol.mong@mail.ru>
2020-08-19 12:50:34 +0700

    123

---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/tflite/ClassifierCatDetector.java
@@ -0,0 +1,96 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+package org.tensorflow.lite.examples.classification.tflite;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+
+import org.tensorflow.lite.support.common.TensorOperator;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp;
+import org.tensorflow.lite.support.image.ops.Rot90Op;
+
+import java.io.IOException;
+
+/** This TensorFlowLite classifier works with the float EfficientNet model. */
+public class ClassifierCatDetector extends Classifier {
+
+  private static final float IMAGE_MEAN = 127.5f;
+  private static final float IMAGE_STD = 127.5f;
+
+  /**
+   * Float model does not need dequantization in the post-processing. Setting mean and std as 0.0f
+   * and 1.0f, repectively, to bypass the normalization.
+   */
+  private static final float PROBABILITY_MEAN = 0.0f;
+
+  private static final float PROBABILITY_STD = 1.0f;
+
+  @Override
+  protected TensorImage loadImage(final Bitmap bitmap, int sensorOrientation) {
+    // Loads bitmap into a TensorImage.
+    inputImageBuffer.load(bitmap);
+
+    // Creates processor for the TensorImage.
+    int cropSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
+    int numRotation = sensorOrientation / 90;
+    // TODO(b/143564309): Fuse ops inside ImageProcessor.
+    ImageProcessor imageProcessor =
+            new ImageProcessor.Builder()
+                    .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
+                    .add(new ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+                    //.add(new Rot90Op(numRotation))
+                    .add(getPreprocessNormalizeOp())
+                    .build();
+    return imageProcessor.process(inputImageBuffer);
+  }
+
+  /**
+   * Initializes a {@code ClassifierFloatMobileNet}.
+   *
+   * @param activity
+   */
+  public ClassifierCatDetector(Activity activity, Device device, int numThreads)
+      throws IOException {
+    super(activity, device, numThreads);
+  }
+
+  @Override
+  protected String getModelPath() {
+    // you can download this file from
+    // see build.gradle for where to obtain this file. It should be auto
+    // downloaded into assets.
+    return "model.tflite";
+  }
+
+  @Override
+  protected String getLabelPath() {
+    return "labels_cats.txt";
+  }
+
+  @Override
+  protected TensorOperator getPreprocessNormalizeOp() {
+    return new NormalizeOp(IMAGE_MEAN, IMAGE_STD);
+  }
+
+  @Override
+  protected TensorOperator getPostprocessNormalizeOp() {
+    return new NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD);
+  }
+}
